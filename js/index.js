e438cb1888139f1d3de9f043d92ff70a8f4e6e52
Julli Babenko <julli.babenko@gmail.com>
2017-05-17 15:02:01 +0000

    js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".news-button button").click(function(){
+        var text = "Вибачте, я поки не працюю. Знаю як реалізувати це за допомогою js та хочу написать це на JQuery ";
+        alert(text);
+    });
+});
